38571a566c2a2994513884a133eccf02706a8c76
= <=>
2023-12-01 01:54:15 +0100

    Initial commit

--- src/components/popupmodal.js
+++ src/components/popupmodal.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactModal from 'react-modal';
-import { bacterial_answer, resistant_answer, positive_resistance, prevent_answer } from './constants'; 
+import ReactModal from 'react-modal'; 
 
 const ModalPopup = ({ isOpen, onRequestClose, question, answer }) => {
   return (
@@ -8,7 +7,7 @@ const ModalPopup = ({ isOpen, onRequestClose, question, answer }) => {
       <div className="flex flex-col items-center justify-center h-full">
         <div class = 'py-8 lg:py-16 px-4 mx-auto max-w-screen-md'>
         <h2 className="font-bold text-center mb-4">{question}</h2>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">{answer}</p>
+        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl display-linebreak">{answer}</p>
         </div>
         <button
           className="text-xl font-bold bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
--- src/physician_constants.js
+++ src/physician_constants.js
@@ -2,7 +2,7 @@ export const bacteria_defensive = `The defensive strategies bacteria use against
 export const how = `Within a large population of bacteria, only a few are resistant. Susceptible bacteria are killed while the resistant bacteria survive\
 Only the drug-resistant bacteria continue to proliferate in the presence of the antibotic and take over. Some bacteria give their drug-resistance to other bacteria,\
 bacteria, causing further increase in drug-resistance in society`;
-export const res_pathogen = `among antibiotic-resistant pathogens, one of the most concerning is Klebsiella pneumoniae (K. pneumoniae). \
+export const res_pathogen = `Among antibiotic-resistant pathogens, one of the most concerning is Klebsiella pneumoniae (K. pneumoniae). \
 It accounts for about one third of all Gram-negative infections, including urinary tract infections, cystitis, pneumonia, surgical wound \
 infections, endocarditis and sepsis.`;
 export const key_role = `As a clinician, you can play a key role in reducing antibiotic resistance by practicing antibiotic stewardship.\
